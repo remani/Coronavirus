2d5c93b9a73bfcc7acfc7185b5d9bcbbe702b577
remani <43198940+remani@users.noreply.github.com>
2020-04-28 10:48:21 -0400

    Update IST361 CoronaVirus Project.py

--- IST361 CoronaVirus Project.py
+++ IST361 CoronaVirus Project.py
@@ -1,22 +1,39 @@
 import urllib.request
+import os
 import tkinter as tk                    
 from tkinter import ttk
 from tkinter import *
+import datetime
+from datetime import date
 import pandas as pd
 
 win = tk.Tk()                       
 win.title("Coronavirus Analytics") 
 tabControl = ttk.Notebook(win)
 downloadDirectory='/Users/r_ema/Downloads/' #This needs to be changed to the individual's download directory
+os.chdir(downloadDirectory)
+
+today = date.today()
+yesterday = today - datetime.timedelta(days=1)
+date = yesterday.strftime("%m/%d/%y").replace('/0', '/')
+if date.startswith("0"):
+    date=date[1:]
+print(date)
 
 def search(event):
-    county=countyName.get()
-    st=state.get()
+    county=countyName.get().strip()
+    st=state.get().strip()
     if county == '' and st == '':
         text1.set("USA")
         text2.set("USA Coronavirus Graph")
         text3.set("USA Coronavirus Statistics")
         text4.set("USA Coronavirus Projections")
+        cases=covidCaseData[date].sum()
+        totalpopulation=countyPopulationData['population'].sum()
+        percentinfected=str(cases/totalpopulation*100)
+        Cases.set(cases)
+        percentInfected.set(percentinfected[:5]+'%')
+        totalPopulation.set(totalpopulation)
     
     elif county == '':
         text1.set(st)
@@ -24,7 +41,7 @@ def search(event):
         text3.set(st+ " Coronavirus Statistics")
         text4.set(st+ " Coronavirus Projections")
     
-    elif len(st)>2 or st.strip().isalpha() is False or county.replace(" ", "").strip().isalpha() is False:
+    elif len(st)>2 or st.isalpha() is False or county.replace(" ", "").isalpha() is False:
         text1.set("**Error: Invalid Entry")
         text2.set("**Error: Invalid Entry")
         text3.set("**Error: Invalid Entry")
@@ -52,6 +69,11 @@ urllib.request.urlretrieve(url, downloadDirectory+ 'covid_deaths_usafacts.csv')
 url = "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_county_population_usafacts.csv"
 urllib.request.urlretrieve(url, downloadDirectory+ 'covid_county_population_usafacts.csv')
 
+covidCaseData = pd.read_csv('covid_confirmed_usafacts.csv')
+
+covidDeathData = pd.read_csv('covid_deaths_usafacts.csv')
+   
+countyPopulationData = pd.read_csv('covid_county_population_usafacts.csv') 
 
 tab1 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
@@ -87,7 +109,16 @@ Label(tab2, textvariable=text2).grid(row=0)
 tabControl.add(tab3, text='Statistics')
 
 text3=StringVar()
+percentInfected=StringVar()
+Cases=StringVar()
+totalPopulation=StringVar()
 Label(tab3, textvariable=text3).grid(row=0)
+Label(tab3, text="Cases:").grid(row=1, column=0)
+Label(tab3, textvariable=Cases).grid(row=1, column=1)
+Label(tab3, text="Percentage Infected:").grid(row=2, column=0)
+Label(tab3, textvariable=percentInfected).grid(row=2, column=1)
+Label(tab3, text="Total Population:").grid(row=3, column=0)
+Label(tab3, textvariable=totalPopulation).grid(row=3, column=1)
 
 #Tab 4
 tabControl.add(tab4, text='Projections')
@@ -97,4 +128,4 @@ Label(tab4, textvariable=text4).grid(row=0)
 
 tabControl.pack(expand=1, fill="both")  
 win.geometry("1000x600")
-win.mainloop()                          
+win.mainloop()  
