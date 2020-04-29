0e6a9ec65b498c5cf148e6be6fa78a48286aa50b
kmcushing <kmcushing01@aol.com>
2020-04-29 16:00:18 -0400

    Implements populateGraphTab
    - graphs data on "Graph" tab

--- IST361 CoronaVirus Project.py
+++ IST361 CoronaVirus Project.py
@@ -1,16 +1,23 @@
 import urllib.request
 import os
-import tkinter as tk                    
+import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 import datetime
 from datetime import date
 import pandas as pd
+import matplotlib as plt
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
-win = tk.Tk()                       
-win.title("Coronavirus Analytics") 
+# Possible additions: autofill the word "county"
+
+win = tk.Tk()
+win.title("Coronavirus Analytics")
 tabControl = ttk.Notebook(win)
-downloadDirectory='/Users/r_ema/Downloads/' #This needs to be changed to the individual's download directory
+# This needs to be changed to the individual's download directory
+downloadDirectory = '/Users/kevincushing/Downloads/'
 os.chdir(downloadDirectory)
 
 today = date.today()
@@ -19,101 +26,146 @@ daybefore = today - datetime.timedelta(days=2)
 daybeforedate = daybefore.strftime("%m/%d/%y").replace('/0', '/')
 yesterdaydate = yesterday.strftime("%m/%d/%y").replace('/0', '/')
 if yesterdaydate.startswith("0"):
-    yesterdaydate=yesterdaydate[1:]
+    yesterdaydate = yesterdaydate[1:]
 if daybeforedate.startswith("0"):
-    daybeforedate=daybeforedate[1:]
+    daybeforedate = daybeforedate[1:]
+
+
+def populateGraphTab(value, dates, f):
+    for w in tab2.winfo_children():
+        w.destroy()
+    data = np.array([])
+    days = np.array([])
+    count = 0
+    for i in dates:
+        if count >= 4:
+            data = np.append(data, f(i))
+            days = np.append(days, (count - 4))
+        count = count + 1
+
+    f = Figure(figsize=(8, 6), dpi=100)
+    p = f.add_subplot(111, yscale="linear",
+                      yticks=np.linspace(0, data[data.size - 1], 10))
+    p.plot(days, data)
+    p.set_xlabel("Days")
+    p.set_ylabel("Cases")
+    p.set_title(value + " COVID-19 Confirmed Cases")
+
+    c = FigureCanvasTkAgg(f, master=tab2)
+    c.draw()
+    c.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+
+def textAssigner(value):
+    text1.set(value)
+    text2.set(value + " COVID-19 Confirmed Cases Graph")
+    text3.set(value + " COVID-19 Statistics")
+    text4.set(value + " COVID-19 Projections")
+
 
 def search(event):
-    county=countyName.get().strip()
-    st=state.get().strip()
+    county = countyName.get().strip()
+    st = state.get().strip()
     if county == '' and st == '':
-        text1.set("USA")
-        text2.set("USA Coronavirus Graph")
-        text3.set("USA Coronavirus Statistics")
-        text4.set("USA Coronavirus Projections")
-        cases=covidCaseData[date].sum()
-        totalpopulation=countyPopulationData['population'].sum()
-        percentinfected=str(cases/totalpopulation*100)
-        deaths=covidDeathData[date].sum()
-        percentdeaths=str(deaths/cases*100)
-        percentpopdeaths=str(deaths/totalpopulation*100)
+        textAssigner("USA")
+        cases = covidCaseData[date].sum()
+        totalpopulation = countyPopulationData['population'].sum()
+        percentinfected = str(cases/totalpopulation*100)
+        deaths = covidDeathData[date].sum()
+        percentdeaths = str(deaths/cases*100)
+        percentpopdeaths = str(deaths/totalpopulation*100)
         percentDeaths.set(percentdeaths[:5]+'%')
         percentPopDeaths.set(percentpopdeaths[:5]+'%')
         Cases.set(cases)
         Deaths.set(deaths)
         percentInfected.set(percentinfected[:5]+'%')
         totalPopulation.set(totalpopulation)
-    
+
+        dates = list(covidCaseData.columns)
+
+        def dataAtIndex(i):
+            return covidCaseData[i].sum()
+        populateGraphTab("USA", dates, dataAtIndex)
+
     elif county == '':
-        text1.set(st)
-        text2.set(st+ " Coronavirus Graph")
-        text3.set(st+ " Coronavirus Statistics")
-        text4.set(st+ " Coronavirus Projections")
+        textAssigner(st)
         stateDataFrame = covidCaseData.loc[covidCaseData['State'] == st]
         deathDataFrame = covidDeathData.loc[covidDeathData['State'] == st]
         populationDataFrame = countyPopulationData.loc[countyPopulationData['State'] == st]
-        totalpopulation=populationDataFrame['population'].sum()
-        cases=stateDataFrame[date].sum()
-        percentinfected=str(cases/totalpopulation*100)
-        deaths=deathDataFrame[date].sum()
-        percentdeaths=str(deaths/cases*100)
-        percentpopdeaths=str(deaths/totalpopulation*100)
+        totalpopulation = populationDataFrame['population'].sum()
+        cases = stateDataFrame[date].sum()
+        percentinfected = str(cases/totalpopulation*100)
+        deaths = deathDataFrame[date].sum()
+        percentdeaths = str(deaths/cases*100)
+        percentpopdeaths = str(deaths/totalpopulation*100)
         percentDeaths.set(percentdeaths[:5]+'%')
-        percentPopDeaths.set(percentpopdeaths[:5]+'%')  
+        percentPopDeaths.set(percentpopdeaths[:5]+'%')
         Cases.set(cases)
         Deaths.set(deaths)
         percentInfected.set(percentinfected[:5]+'%')
         totalPopulation.set(totalpopulation)
-    
-    elif len(st)>2 or st.isalpha() is False or county.replace(" ", "").isalpha() is False:
-        text1.set("**Error: Invalid Entry")
-        text2.set("**Error: Invalid Entry")
-        text3.set("**Error: Invalid Entry")
-        text4.set("**Error: Invalid Entry")
+
+        dates = list(covidCaseData.columns)
+
+        def dataAtIndex(i):
+            return covidCaseData.loc[covidCaseData['State'] == st][i].sum()
+        populateGraphTab(st, dates, dataAtIndex)
+
+    elif len(st) > 2 or st.isalpha() is False or county.replace(" ", "").isalpha() is False:
+        textAssigner("**Error: Invalid Entry")
 
     else:
-        text1.set(county+", "+ st)
-        text2.set(county+ ", "+ st+ " Coronavirus Graph")
-        text3.set(county+ ", "+ st+ " Coronavirus Statistics")
-        text4.set(county+ ", "+ st+ " Coronavirus Projections")
-        countyDataFrame = covidCaseData.loc[(covidCaseData['State'] == st) & (covidCaseData['County Name'] == county)]
-        populationDataFrame = countyPopulationData.loc[(countyPopulationData['State'] == st) & (countyPopulationData['County Name'] == county)]
-        deathDataFrame = covidDeathData.loc[(covidDeathData['State'] == st) & (covidDeathData['County Name'] == county)]
-        totalpopulation=populationDataFrame['population'].sum()
-        deaths=deathDataFrame[date].sum()
-        percentpopdeaths=str(deaths/totalpopulation*100)
-        cases=countyDataFrame[date].sum()
-        percentdeaths=str(deaths/cases*100)
-        percentinfected=str(cases/totalpopulation*100)
+        textAssigner(county + ", " + st)
+        countyDataFrame = covidCaseData.loc[(covidCaseData['State'] == st) & (
+            covidCaseData['County Name'] == county)]
+        populationDataFrame = countyPopulationData.loc[(countyPopulationData['State'] == st) & (
+            countyPopulationData['County Name'] == county)]
+        deathDataFrame = covidDeathData.loc[(covidDeathData['State'] == st) & (
+            covidDeathData['County Name'] == county)]
+        totalpopulation = populationDataFrame['population'].sum()
+        deaths = deathDataFrame[date].sum()
+        percentpopdeaths = str(deaths/totalpopulation*100)
+        cases = countyDataFrame[date].sum()
+        percentdeaths = str(deaths/cases*100)
+        percentinfected = str(cases/totalpopulation*100)
         Cases.set(cases)
         Deaths.set(deaths)
         percentDeaths.set(percentdeaths[:5]+'%')
         percentPopDeaths.set(percentpopdeaths[:5]+'%')
         percentInfected.set(percentinfected[:5]+'%')
         totalPopulation.set(totalpopulation)
-        
-    
-def populateGraphTab():
-    print()
+
+        dates = list(covidCaseData.columns)
+
+        def dataAtIndex(i):
+            return covidCaseData.loc[(covidCaseData['State'] == st) & (
+                covidCaseData['County Name'] == county)][i].sum()
+        populateGraphTab(county + ", " + st, dates, dataAtIndex)
+
 
 def populateStatisticsTab():
     print()
 
+
 def populateProjectionsTab():
     print()
-    
+
+
 url = "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv"
-urllib.request.urlretrieve(url, downloadDirectory+ 'covid_confirmed_usafacts.csv')
+urllib.request.urlretrieve(url, downloadDirectory +
+                           'covid_confirmed_usafacts.csv')
 url = "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_deaths_usafacts.csv"
-urllib.request.urlretrieve(url, downloadDirectory+ 'covid_deaths_usafacts.csv')
+urllib.request.urlretrieve(url, downloadDirectory +
+                           'covid_deaths_usafacts.csv')
 url = "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_county_population_usafacts.csv"
-urllib.request.urlretrieve(url, downloadDirectory+ 'covid_county_population_usafacts.csv')
+urllib.request.urlretrieve(url, downloadDirectory +
+                           'covid_county_population_usafacts.csv')
 
 covidCaseData = pd.read_csv('covid_confirmed_usafacts.csv')
 
 covidDeathData = pd.read_csv('covid_deaths_usafacts.csv')
-   
-countyPopulationData = pd.read_csv('covid_county_population_usafacts.csv') 
+
+countyPopulationData = pd.read_csv('covid_county_population_usafacts.csv')
 
 dates = list(covidCaseData.columns)
 
@@ -121,19 +173,19 @@ if yesterdaydate in dates:
     date = yesterdaydate
 else:
     date = daybeforedate
-    
+
 
 tab1 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
 tab3 = ttk.Frame(tabControl)
 tab4 = ttk.Frame(tabControl)
 
-#Tab 1
+# Tab 1 - Search tab
 tabControl.add(tab1, text='Search')
 
 countyName = StringVar()
 state = StringVar()
-text1=StringVar()
+text1 = StringVar()
 Label(tab1, textvariable=text1).grid(row=3, column=1)
 countyEntry = tk.Entry(tab1, textvariable=countyName).grid(row=0, column=1)
 stateEntry = tk.Entry(tab1, textvariable=state).grid(row=1, column=1)
@@ -147,22 +199,22 @@ searchbutton = tk.Button(tab1, text='Search')
 searchbutton.bind('<Button-1>', search)
 searchbutton.grid(row=2, column=1)
 
-#Tab 2
+# Tab 2 - confirmed cases graph
 tabControl.add(tab2, text='Graph')
 
-text2=StringVar()
-Label(tab2, textvariable=text2).grid(row=0)
+text2 = StringVar()
+# Label(tab2, textvariable=text2).grid(row=0)
 
-#Tab 3
+# Tab 3 - general statistics
 tabControl.add(tab3, text='Statistics')
 
-text3=StringVar()
-percentInfected=StringVar()
-Cases=StringVar()
-Deaths=StringVar()
-percentDeaths=StringVar()
-totalPopulation=StringVar()
-percentPopDeaths=StringVar()
+text3 = StringVar()
+percentInfected = StringVar()
+Cases = StringVar()
+Deaths = StringVar()
+percentDeaths = StringVar()
+totalPopulation = StringVar()
+percentPopDeaths = StringVar()
 Label(tab3, textvariable=text3).grid(row=0)
 Label(tab3, text="Cases:").grid(row=1, column=0)
 Label(tab3, textvariable=Cases).grid(row=1, column=1)
@@ -177,12 +229,12 @@ Label(tab3, textvariable=percentDeaths).grid(row=5, column=1)
 Label(tab3, text="Percentage of Population Dead:").grid(row=6, column=0)
 Label(tab3, textvariable=percentPopDeaths).grid(row=6, column=1)
 
-#Tab 4
+# Tab 4 - projected graph
 tabControl.add(tab4, text='Projections')
 
-text4=StringVar()
+text4 = StringVar()
 Label(tab4, textvariable=text4).grid(row=0)
 
-tabControl.pack(expand=1, fill="both")  
+tabControl.pack(expand=1, fill="both")
 win.geometry("1000x600")
-win.mainloop()           
+win.mainloop()
